350d654f7fcad507e3ae28cc7fbe2c4efab5262f
Franco Petrozzelli <frankitoenzo@gmail.com>
2022-03-02 00:07:38 -0300

    last changes

--- src/components/CityDetails.js
+++ src/components/CityDetails.js
@@ -3,6 +3,8 @@ import UnderConstruction from "./UnderConstruction";
 import axios from 'axios'
 import React, {useEffect, useState} from 'react'
 import {useParams} from 'react-router-dom'
+import { Link as LinkRouter } from "react-router-dom";
+import Button from "react-bootstrap/Button";
 
 
 const CityDetails = () => {
@@ -21,14 +23,33 @@ const CityDetails = () => {
                 
                 
             }).catch(error =>{console.log(error)});
+
+
+            // axios.get(`http://localhost:4000/api/city/${id}`)
+            // .then(response=>{setPrintCities(response.data.response)})
             
             }, []);
     
     return ( 
         <>
         {printCities?.map(city => 
-            <h1 className="d-flex justify-content-center">ESTAS EN {city.place} </h1>
+
+            <div className="HeroDetails d-flex justify-content-center " style={{backgroundImage: `url('${process.env.PUBLIC_URL+"/assets/"+ city.image}' )`}}>
+                
+        <h1 className="cityDetails">{city.place}</h1>
+                
+            </div>
+
         )}
+            
+                <div className="d-flex justify-content-center mt-5">
+                    <LinkRouter to={`/cities`}>
+                        {" "}
+                        <Button className=" btn btn-details btn-warning btn-details-warning btnCityDetails">
+                        Go back to Cities
+                        </Button>{" "}
+                    </LinkRouter>
+                </div>
         <UnderConstruction/>
         </>
     );
--- src/components/SearchNotFound.js
+++ src/components/SearchNotFound.js
@@ -1,18 +1,12 @@
 const SearchNotFound = () => {
     return ( 
-        <div className="d-flex row pt-5 pl-5 searchNotFound mb-5">
+            <div className="d-flex justify-content-center align-items-center notFoundContainer">
+        <div className="d-flex row pl-5 searchNotFound mb-5">
             <div>
-            <h3 className="Searchh3">City not Found</h3>
-            </div>
-            <div className="my-3">
+            <h3 className="Searchh3">CITY NOT FOUND</h3>
             <h4 className="Searchh4">Search another City</h4>
             </div>
-            
-
-
-
-
-
+            </div>
         </div>
     );
 }
