65ee5d4dda98ab20b78c35587dae9b2557ec33bd
Franco Petrozzelli <frankitoenzo@gmail.com>
2022-03-06 17:07:37 -0300

    adding redux

--- src/index.js
+++ src/index.js
@@ -4,15 +4,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux'
+import {createStore, applyMiddleware} from 'redux'
+import mainReducer from './redux/reducers/mainReducer'
+import thunk from 'redux-thunk' 
+
+const reduxStore = createStore(mainReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
+  
   <React.StrictMode>
-    <App />
+    <Provider store={reduxStore}>
+    <App/>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
