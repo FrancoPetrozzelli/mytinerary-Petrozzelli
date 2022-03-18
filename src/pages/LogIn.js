88f224f13c317d99db8599235cf44702cb861143
Franco Petrozzelli <frankitoenzo@gmail.com>
2022-03-17 23:06:41 -0300

    responsive signup/login and some alerts

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -5,24 +5,53 @@ import { Link } from 'react-router-dom'
 import {HiOutlineMail} from "react-icons/hi"
 import {GoLock} from "react-icons/go"
 import FacebookLogIn from "./FacebookLogIn";
+import Swal from "sweetalert2"
+import { useState } from "react";
+
 
 const LogIn= (props) => {
 
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+
 const handleSubmit = (event) => {
     event.preventDefault()
-    const logedUser={
-        email:event.target[0].value,
-        password:event.target[1].value,
-        from:"form-signup"
+
+    if([email,password].includes('')){
+
+        Swal.fire({
+            title:"You didn't complete all the fields",
+            icon: 'error',
+            toast:true,
+            position:'center',
+            background:'#fff'
+        })
+        
+        // Toast.fire({
+        //     icon:'error',
+        // })
+
+    }else{
+
+        const logedUser={
+            email:event.target[0].value,
+            password:event.target[1].value,
+            from:"form-signup"
+        }
+        props.logInUser(logedUser)
+
+
     }
-    props.logInUser(logedUser)
+
+
 }
+
 
 
 return ( 
     <>
-         <div className="d-flex justify-content-center">
-        <div className="hola">
+        <div className="d-flex justify-content-center">
+        <div className="logincontainer">
             <div>
                 <h2>LOG IN</h2>
                 <FacebookLogIn/>
@@ -32,12 +61,12 @@ return (
                 <div className="mb-3 mt-3">
                 {/* <label className="labelLogin">Email:</label> */}
                 <HiOutlineMail className="labelLogin"/>
-                <input type="email" placeholder="email" name="email"/>
+                <input className="inputLogIn" type="email" placeholder="email" name="email" value={email} onChange={ e => setEmail(e.target.value) }/>
                 </div>
-                <div className="mb-3 labelLogin">
+                <div className="mb-3">
                 {/* <label className="labelLogin">Password:</label> */}
                 <GoLock className="labelLogin"/>
-                <input type="password" placeholder="password" name="password"/>
+                <input className="inputLogIn" type="password" placeholder="password" name="password" value={password} onChange={ e => setPassword(e.target.value) }/>
                 </div>
                 <button type="submit" className="btn btn-primary btn-block">Log In</button>
             </form>
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -9,22 +9,49 @@ import {GoLock} from "react-icons/go"
 import {BsFillImageFill} from "react-icons/bs"
 import {BiWorld} from "react-icons/bi"
 import FacebookSignUp from "./FacebookSignUp";
+import Swal from "sweetalert2"
 
 
 function SignUp(props){
 
+    const [firstName, setFirstName] = useState()
+    const [lastName, setLastName] = useState()
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+    const [imageUrl, setImageUrl] = useState()
+    const [country, setCountry] = useState()
+
+
+
 const handleSubmit = (event) => {
     event.preventDefault()
-    const userData={
-        firstName:event.target[0].value,
-        lastName:event.target[1].value,
-        email:event.target[2].value,
-        password:event.target[3].value,
-        imageUrl:event.target[4].value,
-        country:event.target[5].value,
-        from:"form-signup"
+
+
+    if([firstName, lastName, email, password, imageUrl, country].includes('')){
+
+        Swal.fire({
+            title:"You didn't complete all the fields",
+            icon: 'error',
+            toast:true,
+            position:'center',
+            background:'#fff'
+        })
+
+    }else{
+
+        const userData={
+            firstName:event.target[0].value,
+            lastName:event.target[1].value,
+            email:event.target[2].value,
+            password:event.target[3].value,
+            imageUrl:event.target[4].value,
+            country:event.target[5].value,
+            from:"form-signup"
+        }
+        props.signUpUser(userData)
+
     }
-    props.signUpUser(userData)
+
 }
 
 
@@ -48,7 +75,7 @@ const handleSubmit = (event) => {
 return ( 
     
         <div className="d-flex justify-content-center align-items-center containertodotodo">
-        <div className="hola containertodo">
+        <div className="hola">
         <div className="chau">
             <div>
                 <FacebookSignUp/>
@@ -59,39 +86,39 @@ return (
 
                     <div className="mb-3 mt-3">
                 {/* <label className="labelLogin">First Name</label> */}
-                <FaUser className="labelLogin"/>
-                <input type="text" placeholder="First Name" name="firstName" className="inputsignup"/>
+                <FaUser className="labelSignUp"/>
+                <input type="text" placeholder="First Name" name="firstName" className="inputsignup" value={firstName} onChange={ e => setFirstName(e.target.value) }/>
                     </div>
 
                     <div className="mb-3 mt-3">
                 {/* <label className="labelLogin">Last Name</label> */}
-                <HiUserGroup className="labelLogin"/>
-                <input type="text" placeholder="Last Name" name="lastName" className="inputsignup"/>
+                <HiUserGroup className="labelSignUp"/>
+                <input type="text" placeholder="Last Name" name="lastName" className="inputsignup" value={lastName} onChange={ e => setLastName(e.target.value) }/>
                     </div>
 
                     <div className="mb-3 mt-3">
                 {/* <label className="labelLogin">Email</label> */}
-                <HiOutlineMail className="labelLogin"/>
-                <input type="email" placeholder="email@email.com" name="email" className="inputsignup"/>
+                <HiOutlineMail className="labelSignUp"/>
+                <input type="email" placeholder="email@email.com" name="email" className="inputsignup" value={email} onChange={ e => setEmail(e.target.value) }/>
                     </div>
 
                 <div className="mb-3 mt-3">
                 {/* <label className="labelLogin">Password:</label> */}
-                <GoLock className="labelLogin"/>
-                <input type="password" placeholder="Password" name="password" className="inputsignup"/>
+                <GoLock className="labelSignUp"/>
+                <input type="password" placeholder="Password" name="password" className="inputsignup" value={password} onChange={ e => setPassword(e.target.value) }/>
                 </div>
 
                     <div className="mb-3 mt-3 asdasd">
                 {/* <label className="labelLogin">Url Image</label> */}
-                <BsFillImageFill className="labelLogin"/>
-                <input type="url" placeholder="Url profile image" name="imageUrl" className="inputsignup"/>
+                <BsFillImageFill className="labelSignUp"/>
+                <input type="url" placeholder="Url profile image" name="imageUrl" className="inputsignup" value={imageUrl} onChange={ e => setImageUrl(e.target.value) }/>
                     </div>
 
     <div className="countrycontainer">
 
                     <div className="mb-3 mt-3">
-                        <BiWorld className="labelLogin"/>
-                                    <select name="country" className="country" >
+                        <BiWorld className="labelSignUp"/>
+                                    <select name="country" className="country" value={country} onChange={ e => setCountry(e.target.value) }>
                                         <option className="countryoption">Choose your country...</option>
                 {countries.map(country =>(
                     <option value={country.name}>{country.name}</option>
